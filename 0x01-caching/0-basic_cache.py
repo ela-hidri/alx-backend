81ba52271275e68311bc53edf85f60fcc0a8e8b4
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-01-31 20:13:22 +0100

    fixing file

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
-"""Caching"""
-BaseCaching = __import__('base_caching').BaseCaching
+"""
+Caching Basic dictionary
+"""
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ caching system"""
+    """
+    caching system  inherits from BaseCaching
+    """
     def put(self, key, item):
-        """add item to dict"""
+        """
+        add item to dict
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """return value linked to key"""
+        """
+        return value linked to key
+        """
         if key in self.cache_data:
             return self.cache_data[key]
         return None
