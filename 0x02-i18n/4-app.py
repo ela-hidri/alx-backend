5ac1c689eaa7a9965fce87b7e0b6d0bc8da65f65
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-02-06 22:31:21 +0100

    adding 4-app.py

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+init flesk app
+"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+from flask import request
+
+
+class Config:
+    """
+    config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """
+    return page with Hello world
+    """
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    determine the best match with our supported languages.
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(['fr', 'en'])
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
