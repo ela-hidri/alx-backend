9d19096fd49a242bc6d9cefce317c5278f7a0877
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-03-05 23:03:22 +0100

    adding 6-job_process

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,8 +1,8 @@
 const kue = require('kue');
 const queue = kue.createQueue();
 const data = {
-    phoneNumber: '221586425948',
-    message: 'Hello ALX',
+    phoneNumber: '4153518780',
+    message: 'This is the code to verify your account',
 }
 const job = queue.create('push_notification_code', data).save((err) => {
     if (!err)
