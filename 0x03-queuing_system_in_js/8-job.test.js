000b9bf5c4f49621922ff050ea207a24f514644c
ela-hidri <elahidri.eh@gmail.com>
2024-03-06 20:32:46 +0000

    adding changes

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -5,12 +5,10 @@ import { expect } from 'chai';
 
 const queue = kue.createQueue();
 
-const list = [
-    {
-        phoneNumber: '4153518780',
-    message: 'This is the code 1234 to verify your account'
-    }
-];
+const list = [{
+	phoneNumber: '4153518780',
+	message: 'This is the code 1234 to verify your account'
+}];
 describe("createPushNotificationsJobs suite", function(){
     it("should validate jobs inside the queue", function(){
         queue.testMode.enter();
