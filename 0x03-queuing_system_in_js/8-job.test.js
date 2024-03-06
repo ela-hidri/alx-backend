2c9e104f0bb5ca341f7bc5b7c16e45dfcf2fc757
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-03-06 21:36:41 +0100

    adding changes

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -9,13 +9,16 @@ const list = [{
 	phoneNumber: '4153518780',
 	message: 'This is the code 1234 to verify your account'
 }];
+
+
 describe("createPushNotificationsJobs suite", function(){
+    
+    before(() => queue.testMode.enter());
+    afterEach(() => queue.testMode.clear());
+    after(() => queue.testMode.exit());
+
     it("should validate jobs inside the queue", function(){
-        queue.testMode.enter();
         createPushNotificationsJobs(list, queue);
         expect(queue.testMode.jobs.type).to.equal(Array);
-        queue.testMode.clear();
-        queue.testMode.exit();
-
     })
 })
