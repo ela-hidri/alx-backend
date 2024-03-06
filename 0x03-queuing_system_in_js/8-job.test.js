924e8e435f8d879350f08261943a52dfa47efa55
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2024-03-06 21:25:08 +0100

    adding 8-hob-test

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,23 @@
+import kue from 'kue';
+
+import createPushNotificationsJobs from './8-job.js';
+import { expect } from 'chai';
+
+const queue = kue.createQueue();
+
+const list = [
+    {
+        phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account'
+    }
+];
+describe("createPushNotificationsJobs suite", function(){
+    it("should validate jobs inside the queue", function(){
+        queue.testMode.enter();
+        createPushNotificationsJobs(list, queue);
+        expect(queue.testMode.jobs.type).to.equal(Array);
+        queue.testMode.clear();
+        queue.testMode.exit();
+
+    })
+})
